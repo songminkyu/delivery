92c97b063052107589198eb4edff46b610d1c2bb
songminkyu <rabbircarrot@naver.com>
2025-06-05 23:32:10 +0900

    `app/user 모듈 초기 구현 및 인증 기능 추가`

---
+++ apps/user/src/user/entity/user.entity.ts
@@ -0,0 +1,42 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  VersionColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    unique: true,
+  })
+  email: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  age: number;
+
+  @Column()
+  profile: string;
+
+  @Column({
+    select: false,
+  })
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @VersionColumn()
+  version: number;
+}
--- apps/user/src/user/user.controller.ts
+++ apps/user/src/user/user.controller.ts
@@ -4,9 +4,4 @@ import { UserService } from './user.service';
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @Get()
-  getHello(): string {
-    return this.userService.getHello();
-  }
 }
