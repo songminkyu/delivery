9376441622b6045eda2f4dbec03bc04aeff570ca
songminkyu <rabbircarrot@naver.com>
2025-06-06 16:20:54 +0900

    `app/order 모듈 초기 구현 및 주요 엔터티 정의`

---
+++ apps/order/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { OrderModule } from './order/order.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
+import { MongooseModule } from '@nestjs/mongoose';
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        HTTP_PORT: Joi.number().required(),
+        DB_URL: Joi.string().required(),
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow('DB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    OrderModule,
+  ],
+})
+export class AppModule {}
