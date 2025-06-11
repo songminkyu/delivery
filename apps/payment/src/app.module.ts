c9721dc1ded384691670c892ec086f50f9bd4585
songminkyu <rabbircarrot@naver.com>
2025-06-11 18:51:08 +0900

    환경설정 로직 수정: DB 프로덕션 환경에 SSL 설정 추가 및 코드 스타일 개선.

--- apps/payment/src/app.module.ts
+++ apps/payment/src/app.module.ts
@@ -1,54 +1,59 @@
-import { Module } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import * as Joi from 'joi';
-import { PaymentModule } from "./payment/payment.module";
-import { ClientsModule, Transport } from "@nestjs/microservices";
-import { NOTIFICATION_SERVICE, NotificationMicroservice } from "@app/common";
-import { join } from "path";
-import { traceInterceptor } from "@app/common";
+import { PaymentModule } from './payment/payment.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { NOTIFICATION_SERVICE, NotificationMicroservice } from '@app/common';
+import { join } from 'path';
+import { traceInterceptor } from '@app/common';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            isGlobal: true,
-            validationSchema: Joi.object({
-                DB_URL: Joi.string().required(),
-                TCP_PORT: Joi.number().required(),
-                NOTIFICATION_HOST: Joi.string().required(),
-                NOTIFICATION_TCP_PORT: Joi.number().required()
-            })
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_URL: Joi.string().required(),
+        TCP_PORT: Joi.number().required(),
+        NOTIFICATION_HOST: Joi.string().required(),
+        NOTIFICATION_TCP_PORT: Joi.number().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.getOrThrow('DB_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+        ...(configService.get('NODE_ENV') === 'production' && {
+          ssl: {
+            rejectUnauthorized: false,
+          },
         }),
-        TypeOrmModule.forRootAsync({
-            useFactory: (configService: ConfigService) => ({
-                type: 'postgres',
-                url: configService.getOrThrow('DB_URL'),
-                autoLoadEntities: true,
-                synchronize: true,
-            }),
-            inject: [ConfigService]
-        }),
-        ClientsModule.registerAsync({
-            clients: [
-                {
-                    name: NOTIFICATION_SERVICE,
-                    useFactory: (configService: ConfigService) => ({
-                        transport: Transport.GRPC,
-                        options: {
-                            channelOptions: {
-                                interceptors: [traceInterceptor('Payment')],
-                            },
-                            package: NotificationMicroservice.protobufPackage,
-                            protoPath: join(process.cwd(), 'proto/notification.proto'),
-                            url: configService.getOrThrow('NOTIFICATION_GRPC_URL'),
-                        }
-                    }),
-                    inject: [ConfigService]
-                },
-            ],
-            isGlobal: true,
-        }),
-        PaymentModule,
-    ],
+      }),
+      inject: [ConfigService],
+    }),
+    ClientsModule.registerAsync({
+      clients: [
+        {
+          name: NOTIFICATION_SERVICE,
+          useFactory: (configService: ConfigService) => ({
+            transport: Transport.GRPC,
+            options: {
+              channelOptions: {
+                interceptors: [traceInterceptor('Payment')],
+              },
+              package: NotificationMicroservice.protobufPackage,
+              protoPath: join(process.cwd(), 'proto/notification.proto'),
+              url: configService.getOrThrow('NOTIFICATION_GRPC_URL'),
+            },
+          }),
+          inject: [ConfigService],
+        },
+      ],
+      isGlobal: true,
+    }),
+    PaymentModule,
+  ],
 })
-export class AppModule { }
+export class AppModule {}
