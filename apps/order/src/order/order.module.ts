9a3e721fa99daf730855952ad435a3f750959bb0
songminkyu <rabbircarrot@naver.com>
2025-06-15 00:16:43 +0900

    `order` 서비스 리팩토링: 클린 아키텍처 도입 및 gRPC, MongoDB/Mongoose 통합. DTO 및 엔티티 계층 재정의.

--- apps/order/src/order/order.module.ts
+++ apps/order/src/order/order.module.ts
@@ -1,22 +1,46 @@
 import { Module } from '@nestjs/common';
-import { OrderController } from './order.controller';
-import { OrderService } from './order.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import * as Joi from 'joi';
+import { OrderController } from './infrastructure/framework/order.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Order, OrderSchema } from './entity/order.entity';
+import { CreateOrderUseCase } from './usecase/create-order.usecase';
+import { StartDeliveryUseCase } from './usecase/start-delivery.usecase';
+import {
+  OrderDocument,
+  OrderSchema,
+} from './infrastructure/mongoose/entity/order.entity';
+import { OrderRepository } from './infrastructure/mongoose/repository/order.repository';
+import { PaymentGrpc } from './infrastructure/grpc/payment.grpc';
+import { ProductGrpc } from './infrastructure/grpc/product.grpc';
+import { UserGrpc } from './infrastructure/grpc/user.grpc';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       {
-        name: Order.name,
+        name: OrderDocument.name,
         schema: OrderSchema,
       },
-    ])
+    ]),
   ],
   controllers: [OrderController],
-  providers: [OrderService],
+  providers: [
+    CreateOrderUseCase,
+    StartDeliveryUseCase,
+    {
+      provide: 'OrderOutputPort',
+      useClass: OrderRepository,
+    },
+    {
+      provide: 'PaymentOutputPort',
+      useClass: PaymentGrpc,
+    },
+    {
+      provide: 'ProductOutputPort',
+      useClass: ProductGrpc,
+    },
+    {
+      provide: 'UserOutputPort',
+      useClass: UserGrpc,
+    },
+  ],
 })
-export class OrderModule { }
+export class OrderModule {}
